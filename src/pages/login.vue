a205d681c7f574ec7175cdfa3178bea33e38a414
test <485140686@qq.com>
2022-05-30 15:37:36 +0800

    feat: 图标引入
    Element-plus 全局图标引入

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,11 +1,11 @@
 <template>
     <el-row class="min-h-screen bg-indigo-500">
-        <el-col :span="16" class="flex items-center justify-center">
+        <el-col :lg="16" :md="12" class="flex items-center justify-center">
             <div>
                 <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
             </div>
         </el-col>
-        <el-col :span="8" class="bg-light-50 flex items-center justify-center flex-col ">
+        <el-col :lg="8" :md="12" class="bg-light-50 flex items-center justify-center flex-col ">
             <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
             <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
                 <span class="h-[1px] w-16 bg-gray-200"></span>
@@ -14,10 +14,23 @@
             </div>
             <el-form :model="form">
                 <el-form-item>
-                    <el-input v-model="form.username" placeholder="请输入用户名" />
+                    <el-input v-model="form.username" placeholder="请输入用户名">
+                        <template #prefix>
+                            <el-icon class="el-input__icon">
+                                <user />
+                            </el-icon>
+                        </template>
+                    </el-input>
+
                 </el-form-item>
                 <el-form-item>
-                    <el-input v-model="form.password" placeholder="请输入密码" />
+                    <el-input v-model="form.password" placeholder="请输入密码">
+                        <template #prefix>
+                            <el-icon class="el-input__icon">
+                                <lock />
+                            </el-icon>
+                        </template>
+                    </el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button class="w-[250px]" round color="#626aef" type="primary" @click="onSubmit">登录</el-button>
@@ -27,8 +40,7 @@
     </el-row>
 </template>
 <script setup>
-import { reactive } from 'vue'
-
+import { reactive } from 'vue';
 // do not use same name with ref
 const form = reactive({
     username: '',

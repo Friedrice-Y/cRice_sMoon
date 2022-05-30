1ec972dea3db418caa9c4554aa60a613ad6f55c5
test <485140686@qq.com>
2022-05-30 14:54:27 +0800

    feat: 路由配置
    路由基本配置和404页面捕获

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup></script>
 
 <template>
-  <el-button>Default</el-button>
-  <el-button type="primary">Primary</el-button>
-  <el-button type="success">Success</el-button>
-  <el-button type="info">Info</el-button>
-  <el-button type="warning">Warning</el-button>
-  <el-button type="danger">Danger</el-button>
-  <el-button>中文</el-button>
+  <router-view></router-view>
 </template>
 
 <style></style>
---
+++ src/pages/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <el-result icon="warning" title="404" sub-title="你找的页面走丢了~">
+      <template #extra>
+        <el-button type="primary" @click="$router.replace('/')"
+          >回到首页</el-button
+        >
+      </template>
+    </el-result>
+  </div>
+</template>

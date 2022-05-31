1d3d93369b98e07e8d05bf45cc3e6a9b3580b09f
test <485140686@qq.com>
2022-05-31 14:03:07 +0800

    feat: 后台主布局实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
+import Admin from "~/layout/admin.vue";
 import Index from "~/pages/index.vue";
 import Login from "~/pages/login.vue";
 import NotFound from "~/pages/404.vue";
@@ -7,10 +8,16 @@ import NotFound from "~/pages/404.vue";
 const routes = [
   {
     path: "/",
-    component: Index,
-    meta: {
-      title: "后台首页",
-    },
+    component: Admin,
+    children: [
+      {
+        path: "/",
+        component: Index,
+        meta: {
+          title: "后台首页",
+        },
+      },
+    ],
   },
   {
     path: "/login",

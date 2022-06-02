0d885c3e51918e07f064edca1e55089fbb362562
test <485140686@qq.com>
2022-06-02 14:46:38 +0800

    feat: 侧边菜单开发
    样式布局和路由跳转+已知bug修复

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Admin from "~/layout/admin.vue";
 import Index from "~/pages/index.vue";
 import Login from "~/pages/login.vue";
 import NotFound from "~/pages/404.vue";
+import GoodList from "~/pages/goods/list.vue";
 
 const routes = [
   {
@@ -15,6 +16,13 @@ const routes = [
         component: Index,
         meta: {
           title: "后台首页",
+        },
+      },
+      {
+        path: "/goods/list",
+        component: GoodList,
+        meta: {
+          title: "商品管理",
         },
       },
     ],

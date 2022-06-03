717a1f498b8865056e47957b9411aa058b42611a
test <485140686@qq.com>
2022-06-03 22:20:32 +0800

    feat: 数字滚动动画实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
         <el-row :gutter="20">
+            <template v-if="panels.length == 0">
+                <el-col :span="6" v-for="i in 4" :key="i">
+                    <el-skeleton style="width:100%" animated loading>
+                        <template #template>
+                            <el-card shadow="hover" class="border-0">
+                                <template #header>
+                                    <div class="flex justify-between">
+                                        <el-skeleton-item variant="text" style="width: 50%" />
+                                        <el-skeleton-item variant="text" style="width: 10%" />
+                                    </div>
+                                </template>
+                                <el-skeleton-item variant="h3" style="width: 80%" />
+                                <el-divider />
+                                <div class="flex justify-between text-sm text-gray-500">
+                                    <el-skeleton-item variant="text" style="width: 50%" />
+                                    <el-skeleton-item variant="text" style="width: 10%" />
+                                </div>
+                            </el-card>
+
+                        </template>
+                    </el-skeleton>
+                </el-col>
+            </template>
             <el-col :span="6" :offset="0" v-for="(item, index) in panels" :key="index">
                 <el-card shadow="hover" class="border-0">
                     <template #header>
@@ -12,7 +35,7 @@
                         </div>
                     </template>
                     <span class="text-3xl font-bold text-gray-500">
-                        {{ item.value }}
+                        <CountTo :value="item.value"></CountTo>
                     </span>
                     <el-divider />
                     <div class="flex justify-between text-sm text-gray-500">
@@ -27,7 +50,9 @@
     </div>
 </template>
 <script setup>
+import CountTo from '~/components/CountTo.vue';
 import { ref } from 'vue';
+
 import { getStatistics1 } from '~/api/index.js';
 const panels = ref([]);
 getStatistics1().then(res => {

a1257146ca0abb3a5693802b8ee3a427e4bfff61
test <485140686@qq.com>
2022-06-08 21:15:52 +0800

    feat: 公告新增功能实现

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -1,7 +1,7 @@
 <template>
     <el-card shadow="never" class="boder-0">
         <div class="flex items-center mb-4 justify-between">
-            <el-button type="primary" size="small">新增</el-button>
+            <el-button type="primary" size="small" @click="handleCreate">新增</el-button>
             <el-tooltip effect="dark" content="刷新数据" placement="top-start">
                 <el-button text @click="getData">
                     <el-icon :size="20">
@@ -27,23 +27,36 @@
             </el-table-column>
         </el-table>
         <div class="flex items-center justify-center mt-5">
-            <el-pagination background layout="prev,pager,next" :total="total" :current-page="currentPage" :page-size="limit"
-                @current-change="getData" />
+            <el-pagination background layout="prev,pager,next" :total="total" :current-page="currentPage"
+                :page-size="limit" @current-change="getData" />
         </div>
+        <FormDrawer ref="formDrawerRef" title="新增" @submit="handleSubmit">
+            <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
+                <el-form-item label="公告标题" prop="title">
+                    <el-input v-model="form.title" placeholder="公告标题"></el-input>
+                </el-form-item>
+                <el-form-item label="公告内容" prop="content">
+                    <el-input v-model="form.content" placeholder="公告内容" type="textarea" :rows="5"></el-input>
+                </el-form-item>
+            </el-form>
+
+        </FormDrawer>
     </el-card>
 </template>
 <script setup>
-import { ref } from 'vue';
-import { getNoticeList } from "~/api/notice"
+import { reactive, ref } from 'vue';
+import { getNoticeList, createNotice } from "~/api/notice";
+import FormDrawer from '~/components/FormDrawer.vue';
+import { toast } from '~/composables/utils';
 
 // 加载动画
 const loading = ref(false);
+
 
 // 分页
 const currentPage = ref(1);
 const total = ref(0);
 const limit = ref(10);
-
 const tableData = ref([]);
 
 
@@ -64,6 +77,40 @@ getData();
 // 删除
 const handleClick = (id) => {
 
+}
+
+
+// 表单部分
+const formDrawerRef = ref(null);
+const formRef = ref(null);
+const form = reactive({
+    title: "",
+    content: ""
+});
+
+const rules = {
+    title: [{ required: true, message: "公告标题不能为空", trigger: "blur" }],
+    content: [{ required: true, message: "公告内容不能为空", trigger: "blur" }]
+}
+const handleSubmit = () => {
+    formRef.value.validate((valid) => {
+        if (!valid) return
+        formDrawerRef.value.showLoading();
+        createNotice(form).then((res) => {
+            toast("新增成功")
+            getData(1);
+            form.title = "";
+            form.content = "";
+            formDrawerRef.value.close()
+        }).finally(() => {
+            formDrawerRef.value.hideLoading()
+        })
+    })
+}
+
+// 新增
+const handleCreate = () => {
+    formDrawerRef.value.open()
 }
 
 </script>

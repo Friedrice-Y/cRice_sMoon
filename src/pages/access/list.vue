7194b6d11a5a2014dc87ec9d3d036e3757398778
‘FriedRice’ <485140686@qq.com>
2022-06-23 14:46:21 +0800

    feat: 新增和修改菜单权限功能的实现

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card shadow="never" class="border-0">
-    <ListHeader @refresh="getData" />
+    <ListHeader @create="handleCreate" @refresh="getData" />
     <el-tree
       :data="tableData"
       :props="{ label: 'name', children: 'child' }"
@@ -23,7 +23,13 @@
               :active-value="1"
               :inactive-value="0"
             />
-            <el-button text type="primary" size="small">修改</el-button>
+            <el-button
+              text
+              type="primary"
+              size="small"
+              @click.stop="handleEdit(data)"
+              >修改</el-button
+            >
             <el-button text type="primary" size="small">增加</el-button>
             <el-button text type="primary" size="small">删除</el-button>
           </div>
@@ -31,21 +37,113 @@
       </template>
     </el-tree>
   </el-card>
+  <FormDrawer ref="formDrawerRef" title="新增" @submit="handleSubmit">
+    <el-form
+      :model="form"
+      ref="formRef"
+      :rules="rules"
+      label-width="80px"
+      :inline="false"
+    >
+      <el-form-item label="上级菜单" prop="role_id">
+        <el-cascader
+          v-model="form.role_id"
+          :options="options"
+          :props="{
+            label: 'name',
+            children: 'child',
+            checkStrictly: true,
+            emitPath: false,
+            value: 'id',
+          }"
+          placeholder="请选择上级菜单"
+        />
+      </el-form-item>
+      <el-form-item label="菜单/规则" prop="menu">
+        <el-radio-group v-model="form.menu">
+          <el-radio :label="1" border>菜单</el-radio>
+          <el-radio :label="0" border>权限</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="名称" prop="name">
+        <el-input
+          v-model="form.name"
+          placeholder="名称"
+          style="width: 30%"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="菜单图标" prop="icon" v-if="form.menu == 1">
+        <el-input v-model="form.icon" placeholder="菜单图标"></el-input>
+      </el-form-item>
+      <el-form-item
+        label="前端路由"
+        prop="frontpath"
+        v-if="form.menu == 1 && form.role_id > 0"
+      >
+        <el-input v-model="form.frontpath" placeholder="前端路由"></el-input>
+      </el-form-item>
+      <el-form-item label="后端规则" prop="condition" v-if="form.menu == 0">
+        <el-input v-model="form.condition" placeholder="后端规则"></el-input>
+      </el-form-item>
+      <el-form-item label="请求方式" prop="method" v-if="form.menu == 0">
+        <el-select v-model="form.method" placeholder="请选择请求方式">
+          <el-option
+            v-for="item in ['GET', 'POST', 'PUT', 'DELETE']"
+            :key="item"
+            :label="item"
+            :value="item"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="排序" prop="order">
+        <el-input-number v-model="form.order" :min="0" :max="1000" />
+      </el-form-item>
+    </el-form>
+  </FormDrawer>
 </template>
 <script setup>
 import { ref } from "vue";
 import ListHeader from "~/components/ListHeader.vue";
-import { getRuleList } from "~/api/rule";
-import { useInitTable } from "~/composables/useCommon.js";
+import { getRuleList, createRule, updateRule } from "~/api/rule";
+import { useInitTable, useInitForm } from "~/composables/useCommon.js";
+import FormDrawer from "~/components/FormDrawer.vue";
+
+const options = ref([]);
 
 const defaultExpandedKeys = ref([]);
-
 const { loading, tableData, getData } = useInitTable({
   getList: getRuleList,
   onGetListSuccess: (res) => {
+    options.value = res.rules;
     tableData.value = res.list;
     defaultExpandedKeys.value = res.list.map((o) => o.id);
   },
+});
+const {
+  formDrawerRef,
+  formRef,
+  form,
+  rules,
+  drawerTitle,
+  handleSubmit,
+  handleCreate,
+  handleEdit,
+} = useInitForm({
+  form: {
+    rule_id: 0,
+    menu: 0,
+    name: "",
+    condition: "",
+    method: "GET",
+    status: 1,
+    order: 50,
+    icon: "",
+    frontpath: "",
+  },
+  rules: {},
+  getData,
+  update: updateRule,
+  create: createRule,
 });
 </script>
 <style scoped>
